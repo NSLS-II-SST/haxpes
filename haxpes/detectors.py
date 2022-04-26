858962c73361466aba3558951639aaa4d76ba77d
Charles Titus <ctitus@bnl.gov>
2022-04-26 09:55:01 -0400

    F460s added

---
+++ haxpes/detectors.py
@@ -0,0 +1,5 @@
+from sst_base.detectors.f460 import F460
+
+dm3_f460 = F460("XF:07ID-BI{DM3:F4}", name="DM3 F460")
+dm4_f460 = F460("XF:07ID-BI{DM4:F4}", name="DM4 F460")
+dm5_f460 = F460("XF:07ID-BI{DM5:F4}", name="DM5 F460")
